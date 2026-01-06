8f8055cd9de5e2f0c518b2024a3d9deb4177fb65
Jaypal Sapara <48550828+jaypalsapara@users.noreply.github.com>
2026-01-06 16:19:47 +0530

    Update

--- src/Console/Commands/AddCommand.php
+++ src/Console/Commands/AddCommand.php
@@ -2,6 +2,7 @@
 
 namespace BladeElements\Console\Commands;
 
+use GuzzleHttp\Client;
 use Illuminate\Console\Command;
 
 class AddCommand extends Command
@@ -26,7 +27,79 @@ class AddCommand extends Command
     public function handle()
     {
         $component = $this->argument('component');
-        $registry = json_decode(file_get_contents(__DIR__.'/../../registry.json'), true);
-        dd($registry);
+
+        $registry = json_decode(file_get_contents(dirname(__DIR__, 3).'/registry.json'), true);
+
+        $config = $registry[$component] ?? null;
+
+        if (! $config) {
+            $this->error("Component [{$component}] not found");
+
+            return 1;
+        }
+
+        // $this->installDependencies($config);
+
+        $this->installComponent($component, $config);
+    }
+
+    /**
+     * Install component
+     */
+    protected function installComponent(string $component, array $config)
+    {
+        $destination = $this->config('destination');
+
+        foreach ($config['files'] as $file) {
+            $source = $this->config('source').'/'.$file;
+            $info = "[{$component}] component has been added!";
+
+            if (str_contains($file, 'components')) {
+                $destination = $this->config('destination')['ui'].'/'.basename($file);
+            } elseif (str_contains($file, 'classes/')) {
+                $destination = $this->config('destination')['classes'].'/'.basename($file);
+            }
+
+            // Create directory if not exists
+            if (! file_exists(dirname($destination))) {
+                mkdir(dirname($destination), 0755, true);
+            }
+
+            // Ask for override if exists
+            if (file_exists($destination)) {
+                $confirm = $this->ask("Component [{$component}] already exists. Overwrite it? (yes/no) [no]");
+                if (! (strtolower($confirm) === 'y' || strtolower($confirm) === 'yes')) {
+
+                    $this->info("[{$component}] component was skipped.");
+
+                    continue;
+                }
+                $info = "[{$component}] component has been overridden!";
+
+            }
+
+            $client = new Client;
+            $result = $client->get($source, ['headers' => ['Accept' => 'text/plain']]);
+            $content = $result->getBody()->getContents();
+
+            file_put_contents($destination, $content);
+
+            $this->info($info);
+        }
+    }
+
+    /**
+     * Configs
+     */
+    protected function config(string $key)
+    {
+
+        return match ($key) {
+            'source' => 'https://raw.githubusercontent.com/jaypalsapara/blade-elements/main/resources/views',
+            'destination' => [
+                'ui' => resource_path('views/components/ui'),
+                'classes' => app_path('View/Components'),
+            ] ,
+        };
     }
 }
